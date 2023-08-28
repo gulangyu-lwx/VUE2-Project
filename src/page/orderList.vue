4067b65732b162e6994aac4ad2d006e80485faaa
liuwenxiang <1592241085@qq.com>
2023-08-28 19:14:22 +0800

    初次

--- src/page/orderList.vue
+++ src/page/orderList.vue
@@ -1,158 +1,213 @@
 <template>
-    <div class="fillcontain">
-        <head-top></head-top>
-        <div class="table_container">
-            <el-table
-			    :data="tableData"
-			    @expand='expand'
-                :expand-row-keys='expendRow'
-                :row-key="row => row.index"
-			    style="width: 100%">
-			    <el-table-column type="expand">
-			      <template slot-scope="props">
-			        <el-form label-position="left" inline class="demo-table-expand">
-			          <el-form-item label="用户名" >
-			            <span>{{ props.row.user_name }}</span>
-			          </el-form-item>
-			          <el-form-item label="店铺名称">
-			            <span>{{ props.row.restaurant_name }}</span>
-			          </el-form-item>
-			          <el-form-item label="收货地址">
-			            <span>{{ props.row.address }}</span>
-			          </el-form-item>
-			          <el-form-item label="店铺 ID">
-			            <span>{{ props.row.restaurant_id }}</span>
-			          </el-form-item>
-			          <el-form-item label="店铺地址">
-			            <span>{{ props.row.restaurant_address }}</span>
-			          </el-form-item>
-			        </el-form>
-			      </template>
-			    </el-table-column>
-			    <el-table-column
-			      label="订单 ID"
-			      prop="id">
-			    </el-table-column>
-			    <el-table-column
-			      label="总价格"
-			      prop="total_amount">
-			    </el-table-column>
-			    <el-table-column
-			      label="订单状态"
-			      prop="status">
-			    </el-table-column>
-			</el-table>
-            <div class="Pagination" style="text-align: left;margin-top: 10px;">
-                <el-pagination
-                  @size-change="handleSizeChange"
-                  @current-change="handleCurrentChange"
-                  :current-page="currentPage"
-                  :page-size="20"
-                  layout="total, prev, pager, next"
-                  :total="count">
-                </el-pagination>
-            </div>
+  <div>
+    <div>
+      <el-card
+        v-for="(item, index) in items"
+        :key="item.id"
+        shadow="hover"
+        style="display: inline-block; width: 15%; margin: 20px"
+      >
+        <div
+          slot="header"
+          style="display: flex; justify-content: space-between"
+        >
+          <span>{{ item.title }}</span>
+          <span>{{ item.date }}</span>
         </div>
+        <div>{{ item.content }}</div>
+      </el-card>
     </div>
+    <el-pagination
+      v-if="total > pageSize"
+      style="margin-top: 20px; text-align: center"
+      :current-page="currentPage"
+      :page-size="pageSize"
+      :total="total"
+      layout="total, sizes, prev, pager, next, jumper"
+      :page-sizes="[10, 20, 50, 100]"
+      @current-change="handlePageChange"
+    />
+  </div>
 </template>
 
 <script>
-    import headTop from '../components/headTop'
-    import {getOrderList, getOrderCount, getResturantDetail, getUserInfo, getAddressById} from '@/api/getData'
-    export default {
-        data(){
-            return {
-                tableData: [],
-                currentRow: null,
-                offset: 0,
-                limit: 20,
-                count: 0,
-                currentPage: 1,
-                restaurant_id: null,
-                expendRow: [],
-            }
-        },
-    	components: {
-    		headTop,
-    	},
-        created(){
-        	this.restaurant_id = this.$route.query.restaurant_id;
-            this.initData();
-        },
-        mounted(){
-
-        },
-        methods: {
-            async initData(){
-                try{
-                    const countData = await getOrderCount({restaurant_id: this.restaurant_id});
-                    if (countData.status == 1) {
-                        this.count = countData.count;
-                    }else{
-                        throw new Error('获取数据失败');
-                    }
-                    this.getOrders();
-                }catch(err){
-                    console.log('获取数据失败', err);
-                }
-            },
-            handleSizeChange(val) {
-                console.log(`每页 ${val} 条`);
-            },
-            handleCurrentChange(val) {
-                this.currentPage = val;
-                this.offset = (val - 1)*this.limit;
-                this.getOrders()
-            },
-            async getOrders(){
-                const Orders = await getOrderList({offset: this.offset, limit: this.limit, restaurant_id: this.restaurant_id});
-                this.tableData = [];
-                Orders.forEach((item, index) => {
-                    const tableData = {};
-                    tableData.id = item.id;
-                    tableData.total_amount = item.total_amount;
-                    tableData.status = item.status_bar.title;
-                    tableData.user_id = item.user_id;
- 					tableData.restaurant_id = item.restaurant_id;
- 					tableData.address_id = item.address_id;
-                    tableData.index = index;
-                    this.tableData.push(tableData);
-                })
-            },
-            async expand(row, status){
-            	if (status) {
-            		const restaurant = await getResturantDetail(row.restaurant_id);
-	            	const userInfo = await getUserInfo(row.user_id);
-	            	const addressInfo = await getAddressById(row.address_id);
-
-	                this.tableData.splice(row.index, 1, {...row, ...{restaurant_name: restaurant.name, restaurant_address: restaurant.address, address: addressInfo.address, user_name: userInfo.username}});
-                    this.$nextTick(() => {
-                        this.expendRow.push(row.index);
-                    })
-	            }else{
-                    const index = this.expendRow.indexOf(row.index);
-                    this.expendRow.splice(index, 1)
-                }
-            },
-        },
-    }
+export default {
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 10,
+      total: 50,
+      items: [],
+    };
+  },
+  created() {
+    this.getDatas()
+  },
+  methods: {
+    getDatas(curPage=1, curPagesize=10) {
+      setTimeout(() => {
+        this.items = [
+          {
+            id: 1,
+            title: "Card Title 1",
+            date: "2023/08/01",
+            content: "Card content 1",
+          },
+          {
+            id: 2,
+            title: "Card Title 2",
+            date: "2023/08/02",
+            content: "Card content 2",
+          },
+          {
+            id: 3,
+            title: "Card Title 3",
+            date: "2023/08/03",
+            content: "Card content 3",
+          },
+          {
+            id: 4,
+            title: "Card Title 4",
+            date: "2023/08/04",
+            content: "Card content 4",
+          },
+          {
+            id: 5,
+            title: "Card Title 5",
+            date: "2023/08/05",
+            content: "Card content 5",
+          },
+          {
+            id: 6,
+            title: "Card Title 6",
+            date: "2023/08/06",
+            content: "Card content 6",
+          },
+          {
+            id: 7,
+            title: "Card Title 7",
+            date: "2023/08/07",
+            content: "Card content 7",
+          },
+          {
+            id: 8,
+            title: "Card Title 8",
+            date: "2023/08/08",
+            content: "Card content 8",
+          },
+          {
+            id: 9,
+            title: "Card Title 9",
+            date: "2023/08/09",
+            content: "Card content 9",
+          },
+          {
+            id: 10,
+            title: "Card Title 10",
+            date: "2023/08/10",
+            content: "Card content 10",
+          },
+          {
+            id: 11,
+            title: "Card Title 11",
+            date: "2023/08/11",
+            content: "Card content 11",
+          },
+          {
+            id: 12,
+            title: "Card Title 12",
+            date: "2023/08/12",
+            content: "Card content 12",
+          },
+          {
+            id: 13,
+            title: "Card Title 13",
+            date: "2023/08/13",
+            content: "Card content 13",
+          },
+          {
+            id: 14,
+            title: "Card Title 14",
+            date: "2023/08/14",
+            content: "Card content 14",
+          },
+          {
+            id: 15,
+            title: "Card Title 15",
+            date: "2023/08/15",
+            content: "Card content 15",
+          },
+          {
+            id: 16,
+            title: "Card Title 16",
+            date: "2023/08/16",
+            content: "Card content 16",
+          },
+          {
+            id: 17,
+            title: "Card Title 17",
+            date: "2023/08/17",
+            content: "Card content 17",
+          },
+          {
+            id: 18,
+            title: "Card Title 18",
+            date: "2023/08/18",
+            content: "Card content 18",
+          },
+          {
+            id: 19,
+            title: "Card Title 19",
+            date: "2023/08/19",
+            content: "Card content 19",
+          },
+          {
+            id: 20,
+            title: "Card Title 20",
+            date: "2023/08/20",
+            content: "Card content 20",
+          },
+          {
+            id: 21,
+            title: "Card Title 21",
+            date: "2023/08/21",
+            content: "Card content 21",
+          },
+          {
+            id: 22,
+            title: "Card Title 22",
+            date: "2023/08/22",
+            content: "Card content 22",
+          },
+          {
+            id: 23,
+            title: "Card Title 23",
+            date: "2023/08/23",
+            content: "Card content 23",
+          },
+          {
+            id: 24,
+            title: "Card Title 24",
+            date: "2023/08/24",
+            content: "Card content 24",
+          },
+          {
+            id: 25,
+            title: "Card Title 25",
+            date: "2023/08/25",
+            content: "Card content 25",
+          },
+        ];
+        this.items = this.items.slice(10 * (curPage - 1), curPagesize *curPage);
+      }, 1000);
+    },
+    handlePageChange(currentPage) {
+      console.log(currentPage, "currentPagecurrentPage");
+      this.currentPage = currentPage;
+      this.getDatas(currentPage,10)
+      // 发起分页请求获取数据
+      // ...
+    },
+  },
+};
 </script>
-
-<style lang="less">
-	@import '../style/mixin';
-    .table_container{
-        padding: 20px;
-    }
-    .demo-table-expand {
-	    font-size: 0;
-	}
-	.demo-table-expand label {
-	    width: 90px;
-	    color: #99a9bf;
-	}
-	.demo-table-expand .el-form-item {
-	    margin-right: 0;
-	    margin-bottom: 0;
-	    width: 50%;
-	}
-</style>
